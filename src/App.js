dca630328b6d0b251e609ee75289ee80dbddef9f
Bukola Agunbiade <atinsbukol@gmail.com>
2023-08-23 15:45:35 +0100

    Added weather template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
       <h1>Weather App</h1>
+      <Weather/>
       <footer>
         This project was created by Bukola Agunbiade and it is {" "}
       <a href="https://github.com/atinsbukol/react-weather-app" target='_blank' rel='noreferrer'>open-sourced on Github</a>
