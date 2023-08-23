930bfaee7a5016a3e2772fbce2cba5771805d9b7
Bukola Agunbiade <atinsbukol@gmail.com>
2023-08-23 14:06:00 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
       <footer>
         This project was created by Bukola Agunbiade and it is {" "}
       <a href="https://github.com/atinsbukol/react-weather-app" target='_blank' rel='noreferrer'>open-sourced on Github</a>
       </footer>
+      </div>
     </div>
   );
 }
