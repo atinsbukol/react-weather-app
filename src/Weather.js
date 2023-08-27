5c6b506a911020637de093830af4889f48ee4613
Bukola Agunbiade <atinsbukol@gmail.com>
2023-08-27 22:12:28 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -8,7 +8,7 @@ return (
         <form>
             <div className="row">
             <div className="col-9">
-            <input type="search" placeholder="Enter a city.." className="form-control"/>
+            <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on"/>
             </div>
             <div className="col-3">
             <input type="submit" value="search" className=" btn btn-primary w-100" />
@@ -22,10 +22,13 @@ return (
             <li>Wednesday 07:00</li>
             <li>Mostly cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
             <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="weather"/>
-                6°C
+                <div className="clearfix">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="weather" className="float-left"/>
+                <span className="float-left"><span className="temperature ">6</span><span className="unit">°C</span>
+            </span>
+            </div>
             </div>
             <div className="col-6">
                 <ul>
