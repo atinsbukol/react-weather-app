4136e225d3fcfe2bf7fde942613bca6050715dab
Bukola Agunbiade <atinsbukol@gmail.com>
2023-09-08 12:34:14 +0100

    City changed to Lagos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className='container'>
       <h1>Weather App</h1>
-      <Weather defaultCity= "Ife"/>
+      <Weather defaultCity= "Lagos"/>
       <footer>
         This project was created by Bukola Agunbiade and it is {" "}
       <a href="https://github.com/atinsbukol/react-weather-app" target='_blank' rel='noreferrer'>open-sourced on Github</a> and 
